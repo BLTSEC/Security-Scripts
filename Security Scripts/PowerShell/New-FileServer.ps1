8fadae5499d520eb1bdf8c9f0b5a06f5276bfef1
Brennan Turner <darkforest.it@gmail.com>
2017-09-26 14:37:14 -0500

    Comments

--- Security Scripts/PowerShell/New-FileServer.ps1
+++ Security Scripts/PowerShell/New-FileServer.ps1
@@ -9,7 +9,7 @@ While ($Hso.IsListening) {
     $HC = $Hso.GetContext()
     $HRes = $HC.Response
     $HRes.Headers.Add("Content-Type","text/plain")
-    $Buf = [Text.Encoding]::UTF8.GetBytes((GC (Join-Path $Pwd ($HC.Request).RawUrl)))
+    $Buf = [Text.Encoding]::UTF8.GetBytes((Get-Content (Join-Path Get-Location ($HC.Request).RawUrl)))
     $HRes.ContentLength64 = $Buf.Length
     $HRes.OutputStream.Write($Buf,0,$Buf.Length)
     $HRes.Close()
